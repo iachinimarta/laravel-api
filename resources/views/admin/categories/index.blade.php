9d131f637b9f2db4b2b5d0a7744d780dc55136fe
Marta Iachini <iachinimarta@gmail.com>
2022-10-18 00:57:20 +0200

    first commit

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <ul>
+            @foreach ($categories as $category)
+                <li>
+                    <h3>{{$category->name}}</h3>
+                    <p>{{$category->description}}</p>
+                </li>
+            @endforeach
+        </ul>
+        <div class="text-center">
+            <button class="btn btn-info text"><a href="{{route('admin.posts.index')}}"><-</a></button>
+        </div>
+        
+    </div>
+@endsection
