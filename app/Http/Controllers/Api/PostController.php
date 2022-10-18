3de4040029d1395c5bac022134ae0f122439dddd
Marta Iachini <iachinimarta@gmail.com>
2022-10-18 18:59:32 +0200

    Modifiche: - impostazione pagine con paginate()

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::with(['category', 'tags'])->get();
+        $posts = Post::with(['category', 'tags'])->paginate(2);
         return response()->json(
             [
                 'success' => true,
--- resources/js/components/MyMain.vue
+++ resources/js/components/MyMain.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container">
         <h1>All</h1>
-        <div class="row">
+
+        <div v-if="loading" class="d-flex justify-content-center">
+            <div class="spinner-border text-primary" role="status"></div>     
+        </div>
+
+        <div v-else class="row">
             <div class="card col-12 mb-4" v-for="(post, index) in posts" :key="index">
                 <div class="card-body">
                     <h5 class="card-title">{{ post.title }}</h5>
@@ -11,6 +16,24 @@
                 </div>
             </div>
         </div>
+
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -19,14 +42,23 @@
         name: 'MyMain',
         data() {
             return {
-                posts: []
+                posts: [],
+                currentPage: 1,
+                lastPage: null,
+                loading: true
             }
         },
         methods: {
             getPosts() {
+
+                this.loading = true;
+
                 axios.get('api/posts')
                 .then((response) => {
-                    this.posts = response.data.results;
+                    this.posts = response.data.results.data;
+                    this.loading = false;
+                    this.currentPage = response.data.results.current_page;
+                    this.lastPage = response.data.results.last_page;
                 });
             },
             truncateText(text, maxLength) {
